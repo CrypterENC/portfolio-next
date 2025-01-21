38ab4dc81950bdb2c490c1e242d4196cdbddb05b
Nittin Nobby Mathew <a95899003@gmail.com>
2025-01-21 16:27:43 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata: Metadata = {
   title: "𝖢𝗋𝗒𝗉𝗍𝖾𝗋",
   description: "Ｃｒｙｐｔｅｒ' s ＰＯＲＴＦＯＬＩＯ",
